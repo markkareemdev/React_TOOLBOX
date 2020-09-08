495cbeafe4539b7b98484584088eb919431a12d2
Mark <Markkareem100@gmail.com>
2020-09-09 00:04:01 +0100

    first git code

---
+++ src/components/counterComponent.jsx
@@ -0,0 +1,9 @@
+import react, { Component } from "react";
+
+class Counter extends Component {
+  render() {
+    return <h1>Hello world</h1>;
+  }
+}
+
+export default Counter;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+
+const welcomeGreeting = "welcome home";
+ReactDOM.render(welcomeGreeting, document.getElementById("root"));

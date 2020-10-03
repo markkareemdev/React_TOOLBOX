697368cf3ab1751b4ef408703d100baa5333b770
Mark <Markkareem100@gmail.com>
2020-10-04 00:09:17 +0100

    shopping cart taking form

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,16 +1,13 @@
 import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
 class Counter extends Component {
-  state = {
-    value: this.props.counter.value,
-  };
-
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement()}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           increment
@@ -25,19 +22,14 @@ class Counter extends Component {
     );
   }
 
-  handleIncrement = () => {
-    this.setState({ value: this.state.value + 1 });
-    console.log("Increment clicked");
-  };
-
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     return value === 0 ? "zero" : value;
   }
 }
